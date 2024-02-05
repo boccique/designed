08e754e1b9e7f0c2cd74a92de025fe4cf1b9d399
John Yakovl <tchuwipeach@yandex.com>
2024-02-05 20:07:25 +0700

    commit

--- main project for Nasty here/js/menu-leaving.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 121 || document.documentElement.scrollTop > 121) {
-    document.getElementById("navbar").style.opacity = "0";
-  } else {
-    document.getElementById("navbar").style.opacity = "1";
-  }
-}
