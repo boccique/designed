b1a1ea7a37906229ecdc779716c53f46714f5e19
John Yakovl <tchuwipeach@yandex.com>
2024-02-06 19:26:54 +0700

    commit

--- js.js
+++ js.js
@@ -18,6 +18,19 @@ alert(`I am actually ${being}`);*/
 // result = confirm("Are you this dummy and pretty kitty here? 🐈");
 
 
+/*
 let LoveQuestion = prompt(`Do you love me? pick (yes/no)`);
 if(LoveQuestion === `yes`) alert(`I love you too!`);
+*/
 
+/*let name = prompt(`What's your name?`);
+if(name === name){
+    alert(`Hello, ${name}! And welcome to our website!`);
+}*/
+
+let LoveQuestion = prompt(`Do you love me?`)
+if(LoveQuestion === `yes`){
+    alert(`I love you too, baby!`);
+}else{
+    alert(`But i love you...`);
+}
